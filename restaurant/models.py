6768c23f54f6f0c08bcb77ee76e7177246469618
TBerrigan2101 <tberrigan2101@gmail.com>
2023-08-10 17:20:57 +0000

    created initial models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    phone = models.TextField()
+    guests = models.IntegerField(default='')
+    message= models.TextField()
+
+
+    def __str__(self):
+     return self.name
+
+
+class Review(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+ 
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
