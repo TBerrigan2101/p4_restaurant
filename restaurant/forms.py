9e58e405fb76620604f899ab377afcb9c5c1a2c6
TBerrigan2101 <tberrigan2101@gmail.com>
2023-08-15 21:04:22 +0000

    Added bootstrap classes to booking form

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -6,3 +6,12 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ('name','email', 'phone', 'guests', 'message', )
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'guests': forms.TextInput(attrs={'class': 'form-control'}),
+            'message': forms.TextInput(attrs={'class': 'form-control'}),
+
+        }
