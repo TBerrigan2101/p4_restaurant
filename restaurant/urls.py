bcf883a3df77e843b200762c50f866af141c00cd
TBerrigan2101 <tberrigan2101@gmail.com>
2023-08-22 15:13:43 +0000

    Restructured the booking system to using function based views rather than class

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.MenuList.as_view(), name='home'),
-    path('booking', views.BookTable.as_view(), name='booking'),
     path('review', views.LeaveReview.as_view(), name='review'),
+    path('view_booking', views.view_booking, name='view_booking'),
+    path('add_booking', views.add_booking, name='add_booking'),
+    path('edit/<booking_id>', views.edit_booking, name='edit_booking'),
+    path('delete/<booking_id>', views.delete_booking, name='delete_booking'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.views.generic.edit import CreateView
 from django.views import generic, View
 from django.http import HttpResponseRedirect
@@ -8,10 +8,52 @@ from .forms import BookingForm, ReviewForm
 
 # Create your views here.
 
+
+def view_booking(request):
+    bookings = Booking.objects.all()
+    context = {
+        'bookings': bookings
+        }
+    return render(request, 'view_booking.html', context)
+
+
+def add_booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('view_booking')
+    form = BookingForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'add_booking.html', context)  
+
+
+def edit_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    if request.method == 'POST':
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            form.save()
+            return redirect('view_booking')
+    form = BookingForm(instance=booking)
+    context = {
+        'form': form
+    }
+    return render(request, 'edit_booking.html', context)
+
+
+def delete_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking.delete()
+    return redirect('view_booking')
+
+
 class MenuList(generic.ListView):
     model = Menu
     queryset = Menu.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
+    template_name = 'index.html'
     paginate_by = 6
 
 
@@ -19,13 +61,6 @@ class IndexPage(CreateView):
     model = Booking
     form_class = BookingForm
     template_name = 'index.html'
-
-
-class BookTable(CreateView):
-    model = Booking
-    form_class = BookingForm
-    template_name = 'booking.html'
-    success_url = '/'
 
 
 class LeaveReview(CreateView):
@@ -43,3 +78,4 @@ class LeaveReview(CreateView):
                 "review_form": ReviewForm()
             },
         )
+
